46fe2f5776b5767eacfaaa520df53bce36a6dac5
EricMurray-G00423903 <g00423903@atu.ie>
2023-12-12 01:23:56 +0000

    Worked on Startup, added webview background using particles.js, added functionality for checking words.txt and downloading when appropriate, added custom buttons to test and some animations to test out feel

--- Wordle/Wordle/MainPage.xaml.cs
+++ Wordle/Wordle/MainPage.xaml.cs
@@ -1,24 +1,79 @@
-﻿namespace Wordle
+﻿using System.Net.Http;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Wordle
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
+            LoadWordList();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void LoadWordList()
         {
-            count++;
+            // Check if word list exists
+            // If not, download it
+            
+            var localFolder = FileSystem.AppDataDirectory;
+            var localFilePath = Path.Combine(localFolder, "words.txt");
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            
+            if (!File.Exists(localFilePath))
+            {
+                await DownloadWordList(localFilePath);
+            }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            // Add a delay if you want to show the welcome message for a set time
+            await Task.Delay(2000); // e.g., 2 seconds delay
+
+            // Fade out the welcome message
+            await welcomeLabel.FadeTo(0, 1000); // Fades out the label over 1 second
+
+            // Fade in the buttons
+            await Task.WhenAll(
+                newGameButton.FadeTo(1, 500),
+                historyButton.FadeTo(1, 500),
+                settingsButton.FadeTo(1, 500),
+                backgroundWebView.FadeTo(1, 500));
+        }
+
+        private async Task DownloadWordList(string filePath)
+        {
+            var client = new HttpClient();
+            var wordsUrl = "https://raw.githubusercontent.com/DonH-ITS/jsonfiles/main/words.txt";
+
+            try
+            {
+                var response = await client.GetAsync(wordsUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    File.WriteAllText(filePath, content);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions (e.g., network issues)
+                Console.WriteLine($"Error downloading word list: {ex.Message}");
+            }
+        }
+
+        private void OnNewGameClicked(object sender, EventArgs e)
+        {
+            // Navigate to the New Game page
+        }
+
+        private void OnHistoryClicked(object sender, EventArgs e)
+        {
+            // Navigate to the History page
+        }
+
+        private void OnSettingsClicked(object sender, EventArgs e)
+        {
+            // Navigate to the Settings page
         }
     }
 }
